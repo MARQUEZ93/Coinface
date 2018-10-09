3264df169e8c0276d03567ae31bd5f23cd1f10be
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-10-08 23:58:05 -0400

    starting auth

--- frontend/coinface.js
+++ frontend/coinface.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("hit");
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Coinface!</h1>, root);
 });
