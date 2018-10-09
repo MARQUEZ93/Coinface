f7d55f51ece26839b3259c9e9fd75d0ec6c08fb0
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-10-09 19:08:21 -0400

    wallet class

---
+++ app/models/wallet.rb
@@ -0,0 +1,16 @@
+class Wallet < ApplicationRecord
+
+  belongs_to :user, foreign_key: :user_id, class_name: :User
+
+  validates :asset, :user_id, presence: true
+
+  validates :amount, :address, allow_nil: true
+
+  after_initialize :generate_address
+
+  private
+
+  def generate_address
+    self.address = SecureRandom.hex(34)
+  end
+end
