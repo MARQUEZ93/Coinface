20d81d1bba26523856b5cc8a7ea84d34a8f6eb0a
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-10-10 16:16:17 -0400

    login & sign in containers

--- frontend/coinface.js
+++ frontend/coinface.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 //Components
-// import Root from './components/root';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -19,12 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
     delete window.currentUser;
   } else {
     store = configureStore();
-    // we don't put the store directly on the window because
-    // it can be confusing when debugging, sometimes giving you access to state
-    // when you shouldn't
-    window.getState = store.getState;
-    window.dispatch = store.dispatch; // just for testing!
   }
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hello</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
