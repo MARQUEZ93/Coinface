c50f48c432e764cc4dc3a01b3577058b26dfbe85
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-10-15 18:28:34 -0400

    update splash background img

--- frontend/components/Welcome/CenterMessage.js
+++ frontend/components/Welcome/CenterMessage.js
@@ -3,8 +3,11 @@ import React from 'react';
 const CenterMessage = () => {
   return (
     <div className="Welcome-CenterMessage">
-      <p className="Welcome-Buy-pTag-Center"> Buy and sell digital currency </p>
-      <p className="Welcome-pTag"> Coinface is the easiest and most trusted place to buy, sell, and manage your digital currency. </p>
+      <p className="Welcome-Center-Header"> Buy and sell digital currency </p>
+      <div className="Welcome-Center-pTag-Div">
+        <p className="Welcome-Center-pTag"> Coinface is the easiest and most trusted place to buy, </p>
+        <p className="Welcome-Center-pTag"> sell, and manage your digital currency. </p>
+      </div>
       <div className="centerForm">
       <input type="text" className="centerInput" placeholder="Email address" />
         <button className="centerButton" type="button">Get Started</button>
