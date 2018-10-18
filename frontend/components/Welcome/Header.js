65c345206e42a4f0e2e52e37a19c9513e344fa13
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-10-18 17:04:24 -0400

    clean up history.push in header component

--- frontend/components/Welcome/Header.js
+++ frontend/components/Welcome/Header.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
-const Header = () => {
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.historyPush = this.historyPush.bind(this);
+  }
+
+  historyPush(e) {
+    e.preventDefault();
+    if (this.props.location.pathname !== "/") {
+      this.props.history.push("/");
+    }
+  }
+  render() {
+
   return (
       <div className="WelcomeHeader">
-        <div className="Welcome-Logo">
-          <Link className="HeaderLink" to="/"> <p> coinface </p> </Link>
+        <div className="WelcomeLogo">
+          <p className="HeaderLink" onClick={this.historyPush}> coinface </p>
         </div>
         <div className="Welcome-Sessions">
           <Link to="/login" style={{ textDecoration: 'none' }} className="WelcomeLogin"> Sign in </Link>
           <Link to="/signup" style={{ textDecoration: 'none' }} className="WelcomeSignup"> Get started </Link>
         </div>
       </div>
-  );
+    );
+  }
 }
 
-export default Header;
+export default withRouter(Header);
