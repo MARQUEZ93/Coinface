a0e0eabee284f766e4aa66b03dd611e7cdd5b03e
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-10-26 00:19:11 -0400

    restyled get started div

--- frontend/components/Welcome/CustomersServed.js
+++ frontend/components/Welcome/CustomersServed.js
@@ -2,20 +2,22 @@ import React from 'react';
 
 const CustomersServed = () => {
   return (
-      <div className="customersServed">
-        <div className="divCustomersServed">
-          <p>$150B+</p>
-          <p className="littleCustomersServed">Digital currency exchanged</p>
-        </div>
-        <div className="divCustomersServed">
-          <p>32</p>
-          <p className="littleCustomersServed">Countries supported</p>
-        </div>
-        <div className="divCustomersServed">
-          <p>20M+</p>
-          <p className="littleCustomersServed">Customers served</p>
-        </div>
+    <div className="CustomersServed">
+      <div className="threeDivsCS">
+          <div className="divCustomersServed">
+            <p>$150B+</p>
+            <p className="littleCustomersServed">Digital currency exchanged</p>
+          </div>
+          <div className="divCustomersServed">
+            <p>32</p>
+            <p className="littleCustomersServed">Countries supported</p>
+          </div>
+          <div className="divCustomersServed">
+            <p>20M+</p>
+            <p className="littleCustomersServed">Customers served</p>
+          </div>
       </div>
+    </div>
   );
 };
 
