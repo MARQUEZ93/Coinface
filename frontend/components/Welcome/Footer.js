058fc882cc742728771b1df6d9d4834e3ee3f82c
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-11-13 17:53:58 -0500

    angel & linked images in footer

--- frontend/components/Welcome/Footer.js
+++ frontend/components/Welcome/Footer.js
@@ -6,12 +6,22 @@ class Footer extends React.Component {
     return (
         <div className="WelcomeFooter">
           <p className="FooterLogo"> coinface </p>
-          <div className="WelcomeFooterInfo">
-            <a className="FooterATag" href="https://github.com/MARQUEZ93/Coinface">
-              https://github.com/MARQUEZ93/Coinface </a>
-            <a className="FooterATag" href="mailto:alejandroeduardomarquez@gmail.com"><p> alejandroeduardomarquez@gmail.com</p> </a>
+          <div className="footerImages">
+            <a className="footerImage" href="https://github.com/MARQUEZ93/Coinface">
+                <div className="gitImg" id="gitCoin"></div>
+            </a>
+            <a className="footerImage" href="https://linkedin.com/droMarquez">
+                <div className="gitImg" id="linkedCoin"></div>
+            </a>
+            <a className="footerImage" href="https://angel.co/alejandro-eduardo-marquez">
+                <div className="gitImg" id="angelCoin"></div>
+            </a>
           </div>
-          <p className="copyright"> &copy; 2018 Coinface </p>
+          <div className="footerTexts">
+            <a className="copyright" href="mailto:alejandroeduardomarquez@gmail.com"><p> alejandroeduardomarquez@gmail.com</p> </a>
+            <a className="copyright" href="droMarquez.com"><p>droMarquez.com</p></a>
+            <p className="copyright"> &copy; 2018 Coinface </p>
+          </div>
         </div>
     );
   }
