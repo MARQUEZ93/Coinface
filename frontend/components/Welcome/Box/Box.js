68ba036ad9db9d35747c5512c862e586a22eee1f
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-11-15 20:55:11 -0500

    box transform on hover + go to buy onClick

--- frontend/components/Welcome/Box/Box.js
+++ frontend/components/Welcome/Box/Box.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import TopLayer from './TopLayer';
 import MiddleLayer from './MiddleLayer';
 import BottomLayer from './BottomLayer';
 
 class Box extends Component {
 
+  constructor(props) {
+    super(props);
+    this.pushPurchaseSignIn = this.pushPurchaseSignIn.bind(this);
+  }
+
+  pushPurchaseSignIn() {
+    if (this.props.location.pathname !== "/") {
+      this.props.history.push("/buy");
+    } else {
+      this.props.history.push("/login");
+    }
+  }
 
   render() {
     return (
-      <div className="Box">
+      <div onClick={this.pushPurchaseSignIn} className="Box">
         <TopLayer name={this.props.name} src={this.props.src} />
         <MiddleLayer change={this.props.change} price={this.props.price} symbol={this.props.symbol} />
         <BottomLayer sparklines={this.props.sparklines} ymbol={this.props.symbol} color={this.props.color}/>
@@ -17,4 +30,4 @@ class Box extends Component {
   }
 }
 
-export default (Box);
+export default withRouter(Box);
