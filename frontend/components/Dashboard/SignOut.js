9a9035399cf9ef35a345db90ddd7c980c09d5dd5
Alejandro E. Marquez <alejandroeduardomarquez@gmail.com>
2018-11-26 02:53:48 -0500

    see if console log comes up on real site

--- frontend/components/Dashboard/SignOut.js
+++ frontend/components/Dashboard/SignOut.js
@@ -12,6 +12,7 @@ class SignOut extends React.Component {
     e.preventDefault();
     this.props.action().then(response =>
       {
+        console.log(response);
         //when multiple users use guest login on thecoinface.com
         //this allows user to logout despite changed session tokens
         if (response.json[0] == "Nobody signed in") {
